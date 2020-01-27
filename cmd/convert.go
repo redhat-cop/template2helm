92abacf888d3c3ae57b32b66b7552cd939066196
Eric Sauer <esauer@redhat.com>
2020-01-27 14:57:38 -0500

    Fix unmarshalling

--- cmd/convert.go
+++ cmd/convert.go
@@ -1,16 +1,20 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 
 	template "github.com/openshift/api/template/v1"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
 	"helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/chartutil"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/yaml"
 )
 
 const (
@@ -37,11 +41,21 @@ var (
 				return fmt.Errorf("Couldn't load template: %v", err)
 			}
 
-			err = yaml.Unmarshal(yamlFile, &myTemplate)
+			// Convert to json first
+			jsonB, err := yaml.YAMLToJSON(yamlFile)
+			checkErr(err, fmt.Sprintf("Error trasnfoming yaml to json: \n%s", string(yamlFile)))
+
+			err = json.Unmarshal(jsonB, &myTemplate)
 			checkErr(err, "Unable to marshal template")
+
+			//fmt.Println("Printing Template")
+			//fmt.Println(myTemplate)
 
 			// TODO: Convert myTemplate.Objects into individual files
 			var templates []*chart.File
+			err = objectToTemplate(myTemplate.Objects, &templates)
+			checkErr(err, "Failed object to template conversion")
+
 			// TODO: Convert myTemplate.Parameters into a yaml string map
 			var values map[string]interface{}
 
@@ -80,4 +94,25 @@ func checkErr(err error, msg string) {
 		os.Exit(1)
 	}
 	return
+}
+
+// Convert the object list in the openshift template to a set of template files in the chart
+func objectToTemplate(objects []runtime.RawExtension, templates *[]*chart.File) error {
+	//fmt.Println(objects)
+
+	for _, v := range objects {
+		var k8sR unstructured.Unstructured
+		err := json.Unmarshal(v.Raw, &k8sR)
+		if err != nil {
+			return fmt.Errorf(fmt.Sprintf("Failed to unmarshal Raw resource\n%v\n", v.Raw) + err.Error())
+		}
+		log.Printf("Creating a template for object %s", k8sR.GetName())
+		tf := chart.File{
+			Name: k8sR.GetKind() + k8sR.GetName(),
+			Data: v.Raw,
+		}
+		*templates = append(*templates, &tf)
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -5,9 +5,25 @@ go 1.13
 replace github.com/docker/docker => github.com/moby/moby v0.7.3-0.20190826074503-38ab9da00309
 
 require (
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/labstack/gommon v0.3.0
+	github.com/neelance/parallel v0.0.0-20160708114440-4de9ce63d14c // indirect
 	github.com/openshift/api v0.0.0-20200123162640-f263157f58d3
+	github.com/opentracing/basictracer-go v1.0.0 // indirect
+	github.com/opentracing/opentracing-go v1.1.0 // indirect
+	github.com/prometheus/client_golang v1.3.0 // indirect
+	github.com/prometheus/common v0.9.1 // indirect
 	github.com/redhat-cop/dash v0.0.0-20191106154059-10d43304fdc2 // indirect
+	github.com/slimsag/godocmd v0.0.0-20161025000126-a1005ad29fe3 // indirect
+	github.com/sourcegraph/ctxvfs v0.0.0-20180418081416-2b65f1b1ea81 // indirect
+	github.com/sourcegraph/go-langserver v2.0.0+incompatible // indirect
+	github.com/sourcegraph/jsonrpc2 v0.0.0-20191222043438-96c4efab7ee2 // indirect
 	github.com/spf13/cobra v0.0.5
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+	golang.org/x/sys v0.0.0-20200122134326-e047566fdf82 // indirect
+	golang.org/x/tools v0.0.0-20200124200720-1b668f209185 // indirect
 	gopkg.in/yaml.v2 v2.2.8
 	helm.sh/helm/v3 v3.0.2
+	k8s.io/apimachinery v0.17.1
+	sigs.k8s.io/yaml v1.1.0
 )
