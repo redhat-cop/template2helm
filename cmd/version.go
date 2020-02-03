085e4128c271acd44021906a2e0af2ba05e81b51
Eric Sauer <esauer@redhat.com>
2020-02-03 16:25:01 -0500

    Fix typos

--- cmd/version.go
+++ cmd/version.go
@@ -16,8 +16,8 @@ func init() {
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number of Hugo",
-	Long:  `All software has versions. This is Hugo's`,
+	Short: "Print the version number of template2helm",
+	Long:  `All software has versions. This is template2helm's`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(version)
 	},
